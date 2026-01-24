84168a2945044f955f27034ef72ad1b63542ae53
Paul Fanelli <paul.fanelli@gmail.com>
2026-01-24 16:53:06 -0500

    Finally got 3 KVM VMs up and running.
    Chris Marget helped me get 3 KVM VMs up and running. Also got the dhcp
    leasing to always assign the IP addresses to these VMs.

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    libvirt = {
-      source = "dmacvicar/libvirt"
-      version = "0.7.1"
-    }
-  }
-}
-
-provider "libvirt" {
-  uri = "qemu:///system"
-}
-
 # Define the local storage pool
 resource "libvirt_pool" "fedora_pool" {
   name = "fedora_pool"
@@ -18,46 +5,78 @@ resource "libvirt_pool" "fedora_pool" {
   path = "/var/lib/libvirt/images/terraform"
 }
 
+locals {
+  vm_count = 3
+}
+
 # Download the base image
 resource "libvirt_volume" "fedora_base" {
-  name   = "fedora_base.qcow2"
+  count = local.vm_count
+  name   = format("fedora_base_%d.qcow2", count.index)
   pool   = libvirt_pool.fedora_pool.name
   source = var.base_image_path
   format = "qcow2"
 }
 
+data "cloudinit_config" "x" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    filename     = "cloud-config.yaml"
+    content_type = "text/cloud-config"
+
+    content = yamlencode({
+      users = [
+        {
+          name = "fedora"
+          ssh_authorized_keys = [
+            file(pathexpand(var.ssh_public_key_file))
+          ]
+          sudo  = "ALL=(ALL) NOPASSWD:ALL"
+          shell = "/bin/bash"
+        }
+      ]
+    })
+  }
+}
+
 # Cloud-init configuration (for user/ssh keys)
 resource "libvirt_cloudinit_disk" "commoninit" {
-  name           = "commoninit.iso"
-  pool           = libvirt_pool.fedora_pool.name
-  user_data      = data.template_file.user_data.rendered
-  network_config = data.template_file.network_config.rendered
-}
-
-data "template_file" "user_data" {
-  template = file("${path.module}/cloud_init.cfg")
-}
-
-data "template_file" "network_config" {
-  template = file("${path.module}/network_config.cfg")
+  count  = local.vm_count
+  name = format("commoninit_%d.iso", count.index)
+  # name = "commoninit.iso"
+  pool = libvirt_pool.fedora_pool.name
+  user_data      = data.cloudinit_config.x.rendered
+  network_config = <<-EOT
+  version: 2
+  ethernets:
+    eth0:
+      dhcp4: true
+  EOT
 }
 
 # Define the VM
 resource "libvirt_domain" "fedora_vm" {
-  name   = "terraform-fedora"
+  count  = local.vm_count
+  # name   = format("terraform-fedora%d", count.index)
+  name   = count.index == 0 ? "leader" : format("follower%d", count.index)
+  # name   = "terraform-fedora${count.index}"
   memory = "2048"
   vcpu   = 2
 
   network_interface {
-    network_name = "default"
-    wait_for_lease = true  # This forces Terraform to wait for the IP
+    network_name   = "default"
+    # addresses      = ["192.168.122.200"]
+    addresses      = [cidrhost("192.168.122.0/24", 200 + count.index)]
+    wait_for_lease = true # This forces Terraform to wait for the IP
   }
 
   disk {
-    volume_id = libvirt_volume.fedora_base.id
+    volume_id = libvirt_volume.fedora_base[count.index].id
   }
 
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
+  cloudinit = libvirt_cloudinit_disk.commoninit[count.index].id
 
   console {
     type        = "pty"
@@ -73,5 +92,5 @@ resource "libvirt_domain" "fedora_vm" {
 }
 
 output "ip" {
-  value = libvirt_domain.fedora_vm.network_interface[0].addresses[0]
+  value = libvirt_domain.fedora_vm[*].network_interface[0].addresses[0]
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,24 @@
 variable "base_image_path" {
   description = "The path to the base image"
-  type = string
-  default = "https://download.fedoraproject.org/pub/fedora/linux/releases/43/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2"
+  type        = string
+  default     = "https://download.fedoraproject.org/pub/fedora/linux/releases/43/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2"
 }
 
-variable "ssh_public_key_path" {
-  description = "The path to the local SSH public key file"
-  type = string
+#variable "ssh_public_keys" {
+#  description = "The local SSH public keys"
+#  type        = list(string)
+#  default = [
+    #    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC+Y6+H/LxwTxaqACvcy+GDL55C62CXjb2ZDBLj88GFz paul@localhost-live"
+    #    file(pathexpand("~/.ssh/id_ed25519.pub"))
+    #  ]
+    #}
+
+variable "ssh_public_key_file" {
+  description = "The local SSH public key file"
+  type        = string
   default = "~/.ssh/id_ed25519.pub"
+}
+
+output "foo" {
+  value = var.ssh_public_key_file
 }
